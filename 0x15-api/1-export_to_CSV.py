9d23d33fd6bcfade85bb41aeb068ba6d213a7e86
yohelce <yohel.cruz.espinoza@gmail.com>
2022-09-13 02:18:56 -0500

    Added 1-export_to_CSV.py

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Python script that for a given employee ID returns all his todo list"""
+import csv
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    userId = sys.argv[1]
+    user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                        .format(userId))
+    name = user.json().get("username")
+
+    todo = requests.get('https://jsonplaceholder.typicode.com/todos')
+    json_todo = todo.json()
+    with open("{}.csv".format(userId), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [userId, name, a.get("completed"), a.get("title")])
+            for a in json_todo]
