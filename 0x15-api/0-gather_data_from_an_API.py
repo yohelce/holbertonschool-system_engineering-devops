f1fe1f4aad38ab3e5a82eeaa6141780aa6022297
yohelce <yohel.cruz.espinoza@gmail.com>
2022-09-13 00:31:43 -0500

    Fixed line 19

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" script that, using this REST API, for a given employee ID,
+""" Script that, using this REST API, for a given employee ID,
 returns information about his/her TODO list progress."""
 
 import requests
@@ -7,21 +7,23 @@ import sys
 
 
 if __name__ == '__main__':
+    userId = sys.argv[1]
     user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                        .format(sys.argv[1]))
-    name = user.json().get('name')
+                        .format(userId))
+    name = user.json().get("name")
 
     json_todo = requests.get('https://jsonplaceholder.typicode.com/todos')
     totalTasks = 0
     completed = 0
-
-    for task in json_todo:
-        if task.get('userId') == int(userId):
+    titles = []
+    for task in json_todo.json():
+        if task.get("userId") == int(userId):
             totalTasks += 1
-            if task.get('completed'):
+            if task.get("completed"):
                 completed += 1
+                titles.append(task.get("title"))
 
     print('Employee {} is done with task({}/{}):'
           .format(name, completed, totalTasks))
 
-    [print("\t {}".format(title_task)) for title_task in task.get('title')]
+    [print("\t {}".format(title_task)) for title_task in titles]
